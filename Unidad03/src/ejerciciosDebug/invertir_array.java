c3d2c399a31ddd9997e8e2461464afc3ca5af930
Alemol <33874617+alemolamg@users.noreply.github.com>
2020-11-30 11:09:03 +0100

    UP Ejercicio 01 terminado + añadimos archivo pdf con instrucciones.

--- Unidad03/src/ejerciciosDebug/invertir_array.java
+++ Unidad03/src/ejerciciosDebug/invertir_array.java
@@ -7,26 +7,28 @@ public class invertir_array {
 		
 		//declaramos array a invertir y la variable que nos va a servir para
 		//intercambiar los valores del array
-		int array[] = {1,2,3,4,5,6,7,8,9};
+		int array[] = {1,2,3,4,5,6,7,8,9,0};
 		int aux=0;
 		
 		// imprimimos array para comprobar
 		int longitud=array.length;
-//		for (int i = 0; i<=longitud; i++)
-		for (int i = 0; i < longitud; i++)	// 20201126 -eliminamos opción i = array.length	
+		for (int i = 0; i < longitud; i++)	// 20201126 eliminamos opción i = array.length	
 			System.out.print(array[i] + " ");
 		
 			// invertimos array recorriéndolo hasta la mitad e intercambiando valores
 		for(int i=0;i<=longitud/2; i++){	//vamos guardando la posición que vamos a intercambiar desde el final
-			aux = array[longitud-i-1];	// 20201126 -longitud-0 sale del límite, ponemos -1 para mantenerlo
+			if(!(longitud-i-1 == i)){	// 20201130 añadimos if para no cambiar el mismo valor 
+				//aux = array[longitud-i];
+				aux = array[longitud-i-1];	// 20201126 longitud-0 sale del límite, ponemos -1 para mantenerlo
 			
-			//como ya tenemos guardada la posición longiutd-i-1, ya podemos guardar en
-			//esa posición un valor
-			array[longitud-i-1] = array[i];
+				//como ya tenemos guardada la posición longiutd-i-1, ya podemos guardar en
+				//esa posición un valor
+				array[longitud-i-1] = array[i];
 			
-			//guardamos en las posiciones iniciales el valor de aux que ya estaba
-			//guardado
-			array[i] = aux;
+				//guardamos en las posiciones iniciales el valor de aux que ya estaba
+				//guardado
+				array[i] = aux;
+			}
 		}
 		// comprobamos que se ha invertido el array
 		System.out.println();
